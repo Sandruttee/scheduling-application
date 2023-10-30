c0d8ae972380f5ccecfa76cfeb2063e7af425d5c
Sandruttee <sandra.placenyte@gmail.com>
2023-10-30 23:28:57 +0100

    added a footer

--- src/components/Login.js
+++ src/components/Login.js
@@ -46,6 +46,13 @@ const Login = () => {
           </Link>
         </p>
       </form>
+      <footer>
+        <h4>
+          AUTO<span class="red-text">SERVISAS 222E</span>
+        </h4>
+        <p class="p-footer">Mus rasite adresu: Staniūnų g. 67a, Panevėžys</p>
+        <p class="p-footer">Susisiekite su mumis: +37063222439</p>
+      </footer>
     </main>
   );
 };
